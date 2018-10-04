05f06ce4eee181bd04d982905ceca666bc7ac765
Ishan Dutta <ishandutta2007@gmail.com>
2018-10-04 05:52:54 +0530

    added a fake progressbar

--- relaunch_chrome.sh
+++ relaunch_chrome.sh
@@ -1,4 +1,5 @@
 sudo kill -9 $(ps aux | grep 'chromedriver' | awk '{print $2}')
 sudo kill -9 $(ps aux | grep 'Chrome' | awk '{print $2}')
-sleep 30s
+echo 'Relaunching Chrome.'
+bash progressbar.sh
 open -a Google\ Chrome
