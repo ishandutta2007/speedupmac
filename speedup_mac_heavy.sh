2db215dc802a1f07a0a1eac891516c94e3d5a4d1
ishan dutta <ishandutta2007@gmail.com>
2019-07-26 10:51:17 +0530

    Update speedup_mac_heavy.sh

--- speedup_mac_heavy.sh
+++ speedup_mac_heavy.sh
@@ -124,3 +124,6 @@ sudo rm -rf /Users/ishandutta2007/geckodriver.log
 echo "#40. disable the calendar agent\n"
 launchctl unload -w /System/Library/LaunchAgents/com.apple.CalendarAgent.plist
 
+echo "Reverting ownership to ishandutta2007\n"
+chown -R ishandutta2007 /Users/ishandutta2007/Library/Caches
+chown -R ishandutta2007 /Users/ishandutta2007/Library/CachesImportant
