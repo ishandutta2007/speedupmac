d28744574be89c67459c8e76e1fafb399f835af0
ishan dutta <ishandutta2007@gmail.com>
2025-11-05 08:19:06 +0530

    Update remove_large_apps.sh

--- remove_large_apps.sh
+++ remove_large_apps.sh
@@ -1,3 +1,13 @@
+#!/bin/sh -x
+rm -r /Library/Frameworks/Mono.framework
+rm -r /Library/Receipts/MonoFramework-*
+
+for dir in /usr/bin /usr/share/man/man1 /usr/share/man/man3 /usr/share/man/man5; do
+   (cd ${dir};
+    for i in `ls -al | grep /Library/Frameworks/Mono.framework/ | awk '{print $9}'`; do
+      rm ${i}
+    done);
+
 rm -rf ~/Library/Caches/Google/AndroidStudio*.
 rm -rf ~/Library/Android*
 rm -rf ~/.android.
